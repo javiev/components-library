75b6f4fbfe91c9469a4ff4b166f39dd0d4161710
Javier <javier.gomez@consultor.fch.cl>
2023-09-28 17:57:04 -0300

    refactored rollup

--- rollup.config.js
+++ rollup.config.js
@@ -18,11 +18,13 @@ export default [
                 file: packageJson.main,
                 format: "cjs",
                 sourcemap: true,
+                interop: 'compat',
             },
             {
                 file: packageJson.module,
                 format: "esm",
                 sourcemap: true,
+                interop: 'compat',
             },
         ],
         plugins: [
