c7f316cd3e325cce1a9985b1057faf5e3a71c913
Javier <javier.gomez@consultor.fch.cl>
2023-09-28 21:22:02 -0300

    added jest

---
+++ jest-setup.js
@@ -0,0 +1,16 @@
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	moduleNameMapper: {
+		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+	},
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
+};
