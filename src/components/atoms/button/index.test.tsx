0d009a3edf767d1badb82682f67076989b535ffd
Javier <javier.gomez@consultor.fch.cl>
2023-09-28 22:08:59 -0300

    error on test (wip)

--- src/components/atoms/button/index.test.tsx
+++ src/components/atoms/button/index.test.tsx
@@ -1,5 +1,36 @@
-export {};
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Button } from './index';
 
-test('renders learn react link', () => {
-	expect(true).toBe(true);
+describe('Button', () => {
+	it('renders with the correct label', () => {
+		const { getByText } = render(
+			<Button label="Click Me!" color="success" size="large" />
+		);
+		expect(getByText('Click Me!')).toBeInTheDocument();
+	});
+
+	it('triggers the onClick handler when clicked', () => {
+		const handleClick = jest.fn();
+		const { getByText } = render(
+			<Button
+				label="Click Me!"
+				color="success"
+				size="large"
+				onClick={handleClick}
+			/>
+		);
+		fireEvent.click(getByText('Click Me!'));
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+
+	it('renders with the correct color and size', () => {
+		const { getByText } = render(
+			<Button label="Click Me!" color="success" size="large" />
+		);
+		const button = getByText('Click Me!');
+		// Aquí estás verificando las clases CSS aplicadas, ajusta según tu implementación
+		expect(button).toHaveClass('color-success size-large');
+	});
 });
