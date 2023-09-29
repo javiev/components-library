0f7c3cdaa866f6a5bfd4bcb48ddaae5d14dc585a
Javier <javier.gomez@consultor.fch.cl>
2023-09-29 00:32:01 -0300

    jest is running

--- src/components/atoms/button/index.test.tsx
+++ src/components/atoms/button/index.test.tsx
@@ -1,3 +1,10 @@
-test('render a button', () => {
-	expect(true).toBe(true);
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Button } from '.';
+
+describe('Button', () => {
+	test('renders the Button component', () => {
+		render(<Button label="button" color="success" size="medium" />);
+	});
 });
